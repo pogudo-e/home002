26e23eceae3e58aa076be8732869ee7011cd9ec0
Evgeny Pogudo <sensesbit@gmail.com>
2022-08-03 20:38:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿string num = Console.ReadLine();
+
+// Задача 10
+if (num.Length > 2)
+{
+    Console.WriteLine(num[1]);
+}
+
+    Console.WriteLine("");
+
+// задача 13
+if (num.Length > 2)
+{
+    Console.WriteLine(num[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+    Console.WriteLine("");
+
+// Задача 15
+int week;
+Int32.TryParse(num, out week);
+if (week == 6 || week == 7)
+{
+    Console.WriteLine("Да");
+}
+else if(week > 7)
+{
+    Console.WriteLine("Кажется неделя закончилась :)");
+}
+else
+{
+    Console.WriteLine("Нет");
+
+}
